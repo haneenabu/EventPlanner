1cfce43818d784e31762d2cf5eba59e51c2c731e
Haneen AbuKhater <abukhater.haneen@gmail.com>
2017-08-04 11:26:38 -0700

    add functionality to the App.java file to retrive number of guests from user to call calculate guest cost method

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,4 +1,6 @@
+import models.Event;
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
 
 /**
@@ -8,5 +10,18 @@ public class App {
     public static void main(String[] args) {
         BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
         System.out.println("Welcome to Haneen's Event Planning Service!" + '\n' + "You will be prompted with a few event options to get you an accurate event quote!" + '\n');
+        try{
+            //Guest Price
+            System.out.println("Enter the number of your expected attendees: ");
+            int guestNumInput = Integer.parseInt(bufferedReader.readLine());
+            Event event = new Event();
+            int finalCost = event.costOfGuest(guestNumInput);
+            System.out.println("You cost for " + guestNumInput+ " guests would amount to $" + finalCost);
+
+            //Food Price
+
+        }catch (IOException e){
+           e.printStackTrace();
+        }
     }
 }
