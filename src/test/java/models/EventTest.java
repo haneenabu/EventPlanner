10cbf2ab23b1018f370191dfeba575f61af5a320
Haneen AbuKhater <abukhater.haneen@gmail.com>
2017-08-04 08:29:41 -0700

    add the first Test to ensure initial instantiation: passes as expected

--- src/test/java/models/EventTest.java
+++ src/test/java/models/EventTest.java
@@ -1,10 +1,12 @@
 package models;
 
+import org.junit.Test;
 import static org.junit.Assert.*;
 
-/**
- * Created by Guest on 8/4/17.
- */
 public class EventTest {
-
+    @Test
+    public void newEvent_instantiateCorrectly_true () throws Exception{
+        Event testEvent = new Event();
+        assertEquals(true, testEvent instanceof Event);
+    }
 }
