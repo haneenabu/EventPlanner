58a9b2f1e8f980d427d0a641bf412caa5ce7eecd
Haneen AbuKhater <abukhater.haneen@gmail.com>
2017-08-04 11:09:53 -0700

    add the welcome promt to the command line user

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,8 +1,12 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
 /**
  * Created by Guest on 8/4/17.
  */
 public class App {
     public static void main(String[] args) {
-
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        System.out.println("Welcome to Haneen's Event Planning Service!" + '\n' + "You will be prompted with a few event options to get you an accurate event quote!" + '\n');
     }
 }
